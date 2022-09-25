a28e6b608f6f6824f2354071be2d787175ed3f81
oolonglilfox <94095961+oolonglilfox@users.noreply.github.com>
2022-09-25 19:44:04 -0400

    feature: add CTA

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className='CTA' >
+        <a href=''>Download CV</a>
+        <a hidden>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello, I'm</h5>
+        <h1>Yuwei Liu</h1>
+        <h5 className='text-light'>MScAC Student</h5>
+      </div>
+    </header>
   )
 }
 
